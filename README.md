40940aad3bc1f962308862d9db69151bc7fac8ad
高志轩 <gaozhixuan@MacBook-Pro-2.local>
2024-12-19 17:51:26 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Parallel_matching
+# 并行字符串匹配算法
+
+## 运行的环境要求
+
+在运行程序之前，确保安装了所有依赖，您可以通过以下命令安装：
+
+```bash
+pip install -r requirements.txt
+
+
+
+
---
+++ run.sh
@@ -0,0 +1,7 @@
+# pip install -r requirements.txt
+
+python document_retrieval.py
+echo "Document Retrieval Done"
+
+python software_antivirus.py
+echo "Software Antivirus Done"
